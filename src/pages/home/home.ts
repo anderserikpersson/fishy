9c1f05f40b2967c2c4912f58f10dcb955d089b14
anders.erik.persson@gmail.com <anders.erik.persson@gmail.com>
2017-06-21 14:14:26 +0200

    Lagt till karta

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,14 @@
 
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
 
   }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import * as moment from 'moment';
+
+declare var google;
 
 @Component({
   selector: 'page-register',
   templateUrl: 'register.html',
 })
 export class RegisterPage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: new google.maps.LatLng(60.6741537, 17.1342999)
+    });
+
+
 
   username: String;
 
@@ -21,6 +33,8 @@ export class RegisterPage {
       fish:['', Validators.required],
       date: [moment().format()],
       weight: ['0', Validators.required],
+      latitude: ['60.6741537', Validators.required],
+      longitude: ['17.1342999', Validators.required],
     })
     this.username = 'Anders';
     this.catches = db.list(this.username + '/catches');
@@ -31,49 +45,55 @@ export class RegisterPage {
     this.catch.reset();
     
   }
-  addCatchold() {
-    let prompt = this.alertCtrl.create({
-      title: 'Registrera fångst',
-      message: "Mata in uppgifter om din fångst",
-      inputs: [
-        {
-          name: 'fish',
-          placeholder: 'Fisktyp'
-        },
-        {
-          name: 'date',
-          placeholder: 'Fångstdatum'
-        }
-        ,
-        {
-          name: 'comments',
-          placeholder: 'Kommentarer'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Save',
-          handler: data => {
-            this.catches.push({
-              fish: data.fish,
-              date: data.date,
-              comments: data.comments
-            });
-          }
-        }
-      ]
-    });
-    prompt.present();
-  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+    this.loadMap();
   }
+
+  loadMap() {
+    let latLng = new google.maps.LatLng(60.6741537, 17.1342999);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+  }
+
+  addPosition() {
+
+    this.marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+    
+
+    // Ful lösning 
+    let lat = this.marker.position.lat();
+    let lng = this.marker.position.lng();
+    this.catch.get('latitude').setValue(lat);
+    this.catch.get('longitude').setValue(lng);
+
+    let content = "<h4>Koordinater:" + this.map.getCenter() + "</h4>";
+    this.addInfoWindow(content);
+
+  }
+  addInfoWindow(content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(this.marker, 'click', () => {
+      infoWindow.open(this.map, this.marker);
+    });
+
+  }
+
 }
 
