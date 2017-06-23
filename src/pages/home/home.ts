46f0d79e7078caabb8ab2db2c89789329b808a95
anders.erik.persson@gmail.com <anders.erik.persson@gmail.com>
2017-06-23 09:13:29 +0200

    Missade

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,8 +2,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-declare var google;
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
